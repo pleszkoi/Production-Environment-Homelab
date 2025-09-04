b29c833f679c8034a26db13369533c8daa051267
Pleszko Istvan <tyupi20@gmail.com>
2025-09-04 17:06:02 +0200

    using terraform

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Docker network
+resource "docker_network" "app_network" {
+  name = "flask_network"
+}
+
+# Flask app image build
+resource "docker_image" "flask_app" {
+  name = "flask-app:terraform"
+  build {
+    context    = ".."
+    dockerfile = "docker/Dockerfile"
+  }
+}
+
+# Flask app container
+resource "docker_container" "flask_app" {
+  name  = "flask-app-container"
+  image = docker_image.flask_app.image_id
+  ports {
+    internal = 5000
+    external = 5000
+  }
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+}
